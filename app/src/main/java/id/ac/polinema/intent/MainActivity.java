cfb86b921b1ca0945314486bd744ab92b25c68e4
Azzahra Dinda <azzahradindas@gmail.com>
2020-02-17 13:27:38 +0700

    UPDATE - Menambahkan OnClick and Intent

--- app/src/main/java/id/ac/polinema/intent/MainActivity.java
+++ app/src/main/java/id/ac/polinema/intent/MainActivity.java
@@ -1,6 +1,9 @@
 package id.ac.polinema.intent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,4 +15,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void handleExit(View view) {
+        finish();
+    }
+
+    public void handleParcelable(View view) {
+        Intent i =  new Intent(MainActivity.this, ParcelableActivity.class);
+        startActivity(i);
+    }
+
+    public void handleBundle(View view) {
+        Intent i =  new Intent(MainActivity.this, BundleActivity.class);
+        startActivity(i);
+    }
+
+    public void handleImplicit(View view) {
+        Intent i =  new Intent(MainActivity.this, ImplicitIntentActivity.class);
+        startActivity(i);
+    }
+
+    public void handleExplicit(View view) {
+        Intent i =  new Intent(MainActivity.this, ExplicitIntentActivity.class);
+        startActivity(i);
+    }
 }
